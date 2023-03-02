e74d81d9f359f04d9479e7285a7da72f872394d7
Deved Roy <devedroy.dr@gmail.com>
2023-03-02 14:40:57 +0530

    View Interaction without viewModel
    Threading

--- app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
+++ app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.mvvmarchitecture;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
+import com.example.mvvmarchitecture.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    Thread counterThread;
+    int count = 0;
+    boolean isCounterInProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        isCounterInProgress = false;
+        counterThread = new Thread(() -> {
+            while (isCounterInProgress) {
+                try {
+                    Thread.sleep(1000);
+                    count++;
+
+                    binding.tvCounter.post(() -> {
+                        binding.tvCounter.setText("Counter:" + count);
+                    });
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        binding.btnStart.setOnClickListener(v -> {
+            if (!isCounterInProgress) {
+                isCounterInProgress = true;
+                counterThread.start();
+            }
+        });
+
+        binding.btnEnd.setOnClickListener(v -> {
+            isCounterInProgress = false;
+        });
     }
 }
